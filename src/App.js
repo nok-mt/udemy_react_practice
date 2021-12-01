d32565f78c2e2d545f230e466b3189ff534bffc9
nok-mt <79310823+nok-mt@users.noreply.github.com>
2021-12-01 16:12:09 +0900

    memo, useCallback, useMemo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  console.log("Appがレンダリングされました");
+
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
-
   const onChangeText = (event) => setText(event.target.value);
 
-  const onClickOpen = () => setOpen(!open);
+  const onClickOpen = () => setOpen(true);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
 
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button onClick={onClickOpen}>{open ? "非表示" : "表示"}</button>
-      <ChildArea open={open} />
+      <button onClick={onClickOpen} disabled={open && true}>
+        表示
+      </button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
